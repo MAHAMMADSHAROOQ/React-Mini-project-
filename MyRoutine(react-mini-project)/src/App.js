ce753887c4b56155899af3e71c093c3533e1a53b
MAHAMMADSHAROOQ <113004546+MAHAMMADSHAROOQ@users.noreply.github.com>
2022-11-10 17:37:44 +0530

    Add files via upload

---
+++ MyRoutine(react-mini-project)/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import AddWorkouts from "./Components/AddWorkouts";
+import CanvasView from "./Components/CanvasView";
+import Header from "./Components/Header";
+import ViewWorkouts from "./Components/Workouts";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+  <Routes>
+    <Route path="/" element={<ViewWorkouts />} />
+    <Route path="add" element={<AddWorkouts />} />
+    <Route path="graph" element={<CanvasView />} />
+  </Routes>
+    </BrowserRouter>
+    
+  );
+}
+
+
+
